69d3efb17028c8761ab32c784e289f9168445565
chaksack <chakdahah@gmail.com>
2025-10-15 13:11:44 +0000

    version 2.1.2

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -46,7 +46,7 @@ func SetupRoutes(
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
 			"message":       "Welcome to Montessori World Connect API",
-			"version":       "2.1.1",
+			"version":       "2.1.2",
 			"documentation": "/swagger/index.html",
 		})
 	})
